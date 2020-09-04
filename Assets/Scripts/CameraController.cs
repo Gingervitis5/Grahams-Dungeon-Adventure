6a1f916af70d15f515c4d45ef9a3a86c22e4ba0b
Graham Mix <gmix@eagles.ewu.edu>
2020-09-03 21:12:13 -0700

    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    private float zoom = 10f;
+    public float zoomSpeed = 4f;
+    public float zoomMin = 5f;
+    public float zoomMax = 15f;
+    public float pitch = 2f;
+
+    public float yawSpeed = 100f;
+    public float currentYaw = 0f;
+
+    void Update() {
+        zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate() {
+        transform.position = target.position - offset * zoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+}
